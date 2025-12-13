c9a732ab7454bff62f12bf3d72619dceb85fd56a
Akash-Parmar36 <aakashsinghparmar36@gmail.com>
2025-12-13 15:02:39 +0530

    fade out animation in Login

--- dashboard/src/components/Login.js
+++ dashboard/src/components/Login.js
@@ -33,20 +33,32 @@ const Login = () => {
          const response = await axios.post("https://fininvest-backend.onrender.com/auth/login" , {email , password});
         //  console.log(response.data);
           
-         if(response.data.success){
-           toast.success(response.data.message , {
-            duration: 2500
-           });
-         }
- 
+        //  if(response.data.success){
+        //    toast.success(response.data.message , {
+        //     duration: 2500
+        //    });
+        //  }
+         
+          if(response.data.success){
+            toast.success(response.data.message);
+          }
+
          setAuth({user: response.data.name , token: response.data.jwtToken});
          localStorage.setItem("authInfo" , JSON.stringify(response.data));
          
-  
          setTimeout(() => {
-             setIsLoading(false);
+          document.body.classList.add("fade-out");
+    
+          // ⏱️ Wait for fade animation
+          setTimeout(() => {
              navigate("/"); 
-         }, 2800);
+          }, 500);
+        }, 1500);
+
+        //  setTimeout(() => {
+        //      setIsLoading(false);
+        //      navigate("/"); 
+        //  }, 2800);
 
    }catch(error){
       console.log(error);
